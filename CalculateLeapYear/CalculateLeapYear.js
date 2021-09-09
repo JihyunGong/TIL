91f6da959fad03d40ba295d1d328368ded11043b
Dana <88199458+JihyunGong@users.noreply.github.com>
2021-09-09 23:25:18 +0900

    Update CalculateLeapYear.js

--- CalculateLeapYear/CalculateLeapYear.js
+++ CalculateLeapYear/CalculateLeapYear.js
@@ -13,7 +13,3 @@ for(var i = 0; i < year.length; i++){
 // "2004 year is Leap Year."
 // "2100 year is not Leap Year."
 // "2021 year is not Leap Year."
-
-//윤년 계산법: 연도를 4로 나누어 나머지가 0으로 떨어지는 해는 윤년이다. 그러나, 그 중 100으로 나누어 떨어지는 해는 윤년이 아니다. 
-//            앞의 조건이 참이건 거짓이건 연도를 400으로 나누어 떨어지는 해는 윤년이다. 
-
